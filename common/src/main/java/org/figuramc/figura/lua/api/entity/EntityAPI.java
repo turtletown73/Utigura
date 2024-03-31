a2e55fc59cb3593e95080079a86b5531c8aa8a09
Avatcher <pravdindmytro@gmail.com>
2024-04-01 01:56:13 +0200

    Remove unused `LadderBlock` import

--- common/src/main/java/org/figuramc/figura/lua/api/entity/EntityAPI.java
+++ common/src/main/java/org/figuramc/figura/lua/api/entity/EntityAPI.java
@@ -14,7 +14,6 @@ import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.entity.vehicle.ContainerEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.LadderBlock;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
