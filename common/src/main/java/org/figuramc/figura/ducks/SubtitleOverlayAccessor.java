3c6850470e56c5aebf650867267c65d73e0a9bac
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2024-06-07 22:06:40 -0600

    Fix Subtitles not displaying for scripted sounds Fix on play sound only working for ui sounds

--- common/src/main/java/org/figuramc/figura/ducks/SubtitleOverlayAccessor.java
+++ common/src/main/java/org/figuramc/figura/ducks/SubtitleOverlayAccessor.java
@@ -3,5 +3,5 @@ package org.figuramc.figura.ducks;
 import org.figuramc.figura.lua.api.sound.LuaSound;
 
 public interface SubtitleOverlayAccessor {
-    void figura$PlaySound(LuaSound sound);
+    void figura$PlaySound(LuaSound sound, float volume);
 }
