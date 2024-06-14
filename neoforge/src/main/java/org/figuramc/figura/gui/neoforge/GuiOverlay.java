3ed3ea945d73b37cad9d1f242e6705ebf46f368b
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2024-06-13 19:06:10 -0600

    Re-enable Neoforge

--- neoforge/src/main/java/org/figuramc/figura/gui/neoforge/GuiOverlay.java
+++ neoforge/src/main/java/org/figuramc/figura/gui/neoforge/GuiOverlay.java
@@ -1,13 +1,16 @@
 package org.figuramc.figura.gui.neoforge;
 
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.LayeredDraw;
 import org.figuramc.figura.avatar.AvatarManager;
 import org.figuramc.figura.gui.FiguraGui;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public class GuiOverlay implements LayeredDraw.Layer {
     @Override
-    public void render(GuiGraphics guiGraphics, float f) {
+    public void render(@NotNull GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
         if (!AvatarManager.panic)
             FiguraGui.renderOverlays(guiGraphics);
     }
--- neoforge/src/main/java/org/figuramc/figura/gui/neoforge/GuiUnderlay.java
+++ neoforge/src/main/java/org/figuramc/figura/gui/neoforge/GuiUnderlay.java
@@ -1,15 +1,16 @@
 package org.figuramc.figura.gui.neoforge;
 
 
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.LayeredDraw;
 import org.figuramc.figura.gui.FiguraGui;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public class GuiUnderlay implements LayeredDraw.Layer {
-
     @Override
-    public void render(GuiGraphics guiGraphics, float tickDelta) {
-        FiguraGui.onRender(guiGraphics, tickDelta, new CallbackInfo("dummy", true));
+    public void render(@NotNull GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+        FiguraGui.onRender(guiGraphics, deltaTracker.getGameTimeDeltaPartialTick(false), new CallbackInfo("dummy", true));
     }
 }
